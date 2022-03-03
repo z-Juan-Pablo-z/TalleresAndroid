ead1eba2a7b063e490d27f8a2f354ced7631ac2d
206 <206@academicos.cesde.edu.co>
2022-03-03 08:53:43 -0500

    Clase03marzo

--- Concesionario/app/src/main/java/com/example/concesionario/Cliente.java
+++ Concesionario/app/src/main/java/com/example/concesionario/Cliente.java
@@ -2,6 +2,10 @@ package com.example.concesionario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +17,8 @@ public class Cliente extends AppCompatActivity {
     EditText etidentificacion,etnombre,etcorreo,etcontraseña,etcontraseña2;
     Button btnguardar,btnconsultar,btnanular,btncancelar,btnregresar;
     TextView tvactivo;
+    long resp;
+    int sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,7 @@ public class Cliente extends AppCompatActivity {
         btncancelar = findViewById(R.id.btnCancelar);
         btnregresar = findViewById(R.id.btnRegresar);
         tvactivo = findViewById(R.id.tvactivo);
+        sw=0;
     }
      public void Guardar(View view) {
         String identificacion,nombre,correo,clave1,clave2;
@@ -43,15 +50,78 @@ public class Cliente extends AppCompatActivity {
              Toast.makeText(this, "TODOS LOS DATOS SON REQUERIDOS.", Toast.LENGTH_SHORT).show();
              etidentificacion.requestFocus();
          }else{
-             if (clave1.equals(clave2)){
+             if (!clave1.equals(clave2)){
                  Toast.makeText(this, "Las claves no coinciden", Toast.LENGTH_SHORT).show();
                  etcontraseña2.requestFocus();
              }else{
                  Conexion_Consesionario admin = new Conexion_Consesionario(this,"concecionario.bd",null,1);
+                 SQLiteDatabase db=admin.getWritableDatabase();
+                 ContentValues dato = new ContentValues();
+                 dato.put("Identificacion",identificacion);
+                 dato.put("nombre",nombre);
+                 dato.put("usuario",correo);
+                 dato.put("clave",clave1);
+                 if(sw==0){
+                     resp=db.insert("TblCliente",null,dato);
+                 }else {
+                     sw = 0;
+                     resp = db.update("TblCliente",dato, "identificacion= '"+identificacion+"'",null);
+                 }
+                 if (resp>0){
+                     Toast.makeText(this, "Registro guardado", Toast.LENGTH_SHORT).show();
+                     limpiarCampos();
+                 }else{
+                     Toast.makeText(this, "Error guardando registro", Toast.LENGTH_SHORT).show();
+                 }
+                 db.close();
              }
          }
 
 
 
+     }
+
+    public void limpiarCampos(){
+        sw=0;
+        etidentificacion.setText("");
+        etidentificacion.requestFocus();
+        etcontraseña.setText("");
+        etcorreo.setText("");
+        etcontraseña2.setText("");
+        etnombre.setText("");
+
+
+
+     }
+     public void consultar(View view){
+        consultarCliente();
+     }
+
+     public void consultarCliente(){
+        String identificacion;
+        identificacion = etidentificacion.getText().toString();
+         if(identificacion.isEmpty()){
+             Toast.makeText(this, "Identificacion requerida", Toast.LENGTH_SHORT).show();
+             etidentificacion.requestFocus();
+         }else{
+             Conexion_Consesionario admin = new Conexion_Consesionario(this,"concecionario.bd",null,1);
+             SQLiteDatabase db =admin.getReadableDatabase();
+             Cursor fila = db.rawQuery("Select * from TblCliente where identificacion='"+identificacion+"'",null);
+             if (fila.moveToNext()){
+                 sw=1;
+                 Toast.makeText(this,"Registro encontrado",Toast.LENGTH_SHORT).show();
+                 etnombre.setText(fila.getString(1));
+                 etcorreo.setText(fila.getString(2));
+                 etcontraseña.setText(fila.getString(3));
+
+             }else{
+                 Toast.makeText(this, "Registro no existe", Toast.LENGTH_SHORT).show();
+             }
+             db.close();
+         }
+     }
+     public void regresar(View view){
+         Intent intmain = new Intent(this,MainActivity.class);
+         startActivity(intmain);
      }
 }
--- Concesionario/app/src/main/java/com/example/concesionario/MainActivity.java
+++ Concesionario/app/src/main/java/com/example/concesionario/MainActivity.java
@@ -2,13 +2,69 @@ package com.example.concesionario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUsuario,etContraseña;
+    Button btnIngresar,btnCancelar,btnRegistrarse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        etUsuario=findViewById(R.id.etUsuario);
+        etContraseña=findViewById(R.id.etContraseña);
+        btnIngresar=findViewById(R.id.btnIngresar);
+        btnRegistrarse=findViewById(R.id.btnRegistrar);
+        btnCancelar=findViewById(R.id.btnCancelar);
+    }
+
+    public void Registrarse(View view){
+        Intent intRegistrarse=new Intent(this,Cliente.class);
+        startActivity(intRegistrarse);
+    }
+    public void cancelar(View view){
+        limpiarCampos();
+    }
+    public void limpiarCampos(){
+        etContraseña.setText("");
+        etUsuario.setText("");
+        etUsuario.requestFocus();
+    }
+    public void Ingresar(View view){
+        String usuario;
+        String clave;
+        clave = etContraseña.getText().toString();
+        usuario = etUsuario.getText().toString();
+        if (usuario.isEmpty()||clave.isEmpty()){
+            Toast.makeText(this, "Necesita un usuario y contraseña", Toast.LENGTH_SHORT).show();
+            etUsuario.requestFocus();
+        }else{
+            Conexion_Consesionario admin = new Conexion_Consesionario(this,"concecionario.bd",null,1);
+            SQLiteDatabase db = admin.getReadableDatabase();
+            Cursor fila = db.rawQuery("Select usuario from TblCliente where usuario='"+usuario+"'and clave = '"+clave+"'"  ,null);
+            if (fila.moveToNext()){
+                Toast.makeText(this, "Usuario coincide", Toast.LENGTH_SHORT).show();
+                Intent intVehiculo= new Intent(this,Vehiculo.class);
+                startActivity(intVehiculo);
+
+            }else{
+                Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_SHORT).show();
+                etUsuario.requestFocus();
+            }
+            db.close();
+        }
+
+
     }
 }
